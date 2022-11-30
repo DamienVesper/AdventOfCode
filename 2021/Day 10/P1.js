dfec4e131c08524255265ca6ca170849f1f6e17a
Damien Vesper <ldamienvesper@gmail.com>
2022-11-30 01:06:29 -0600

    2021 day 10

---
+++ 2021/Day 10/P1.js
@@ -0,0 +1,52 @@
+const fs = require(`fs`);
+const path = require(`path`);
+
+const pts = {
+    [`)`]: 3,
+    [`]`]: 57,
+    [`}`]: 1197,
+    [`>`]: 25137
+}
+
+const pairs = {
+    [`(`]: `)`,
+    [`[`]: `]`,
+    [`{`]: `}`,
+    [`<`]: `>`
+}
+
+const openings = Object.keys(pairs);
+
+const errs = {
+    [`)`]: 0,
+    [`]`]: 0,
+    [`}`]: 0,
+    [`>`]: 0
+};
+
+const main = async () => {
+    fs.readFile(path.resolve(__dirname, `./input.txt`), `utf-8`, (err, data) => {
+        if (err) throw err;
+        
+        const lines = data.split(`\n`).map(x => x.trim());
+        for (const line of lines) {
+            const opened = [];
+
+            const tokens = line.split(``);
+            for (const token of tokens) {
+                if (openings.includes(token)) opened.push(token);
+                else if (pairs[opened.pop()] !== token) {
+                    errs[token]++;
+                    break;
+                }
+            }
+        }
+
+        let total = 0;
+        for (const [token, count] of Object.entries(errs)) total += pts[token] * count;
+
+        console.log(`Result: ${total}`);
+    });
+};
+
+void main();
---
+++ 2021/Day 10/P2.js
@@ -0,0 +1,58 @@
+const fs = require(`fs`);
+const path = require(`path`);
+
+const pts = {
+    [`)`]: 1,
+    [`]`]: 2,
+    [`}`]: 3,
+    [`>`]: 4
+}
+
+const pairs = {
+    [`(`]: `)`,
+    [`[`]: `]`,
+    [`{`]: `}`,
+    [`<`]: `>`
+}
+
+const openings = Object.keys(pairs);
+
+const main = async () => {
+    fs.readFile(path.resolve(__dirname, `./input.txt`), `utf-8`, (err, data) => {
+        if (err) throw err;
+
+        
+        const lines = data.split(`\n`).map(x => x.trim());
+        const totals = [];
+
+        for (const line of lines) {
+            let total = 0;
+
+            const opened = [];
+            const tokens = line.split(``);
+
+            let isCorrupted = false;
+            for (const token of tokens) {
+                if (openings.includes(token)) opened.push(token);
+                else if (pairs[opened.pop()] !== token) {
+                    isCorrupted = true;
+                    break;
+                }
+            }
+
+            if (isCorrupted) continue;
+
+            for (const token of opened.reverse()) {
+                total *= 5;
+                total += pts[pairs[token]];
+            }
+
+            totals.push(total);
+        }
+
+        const total = totals.sort((a, b) => a - b)[Math.floor(totals.length / 2)];
+        console.log(`Result: ${total}`);
+    });
+};
+
+void main();
