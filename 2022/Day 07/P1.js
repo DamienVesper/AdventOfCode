2e87208317393d15317e318cc9f83eeb07094c79
Damien Vesper <ldamienvesper@gmail.com>
2022-12-07 00:07:40 -0600

    2022 day 07

---
+++ 2022/Day 07/P1.js
@@ -0,0 +1,77 @@
+const fs = require(`fs`);
+const path = require(`path`);
+
+const expand = path => {
+    if (path === `/`) return [`/`];
+
+    const parts = path.split(`/`);
+    const expanded = [`/`];
+
+    for (let i = 1; i < parts.length; i++) expanded.push(`${parts.slice(0, i + 1).join(`/`)}`);
+    return expanded;
+}
+
+const main = async () => {
+    let tree = {};
+    let curPath = `/`;
+
+    const parseCommand = line => {
+        const args = line.split(/\s/g);
+        const command = args.shift();
+
+        if (command === `cd`) {
+            const path = args[0];
+
+            if (path.startsWith(`/`)) curPath = path;
+            else if (path.startsWith(`..`)) {
+                const parts = curPath.split(`/`);
+                parts.pop();
+
+                curPath = parts.join(`/`);
+                if (curPath === ``) curPath = `/`;
+            } else {
+                if (curPath[curPath.length - 1] === `/`) curPath += path;
+                else curPath += `/${path}`;
+            }
+
+            if (tree[curPath] === undefined) tree[curPath] = 0;
+        }
+    };
+
+    fs.readFile(path.resolve(__dirname, `./input.txt`), `utf-8`, (err, data) => {
+        if (err) throw err;
+
+        const lines = data.split(`\n`);
+        
+        for (const line of lines) {
+            if (line.startsWith(`$`)) parseCommand(line.slice(2));
+            else {
+                const [size, name] = line.split(/\s/g);
+                if (size === `dir`) {
+                    const fName = path.join(curPath, name).replace(/\\/g, `/`);
+                    if (tree[fName] === undefined) tree[fName] = 0;
+                }
+            }
+        }
+
+        for (const line of lines) {
+            if (line.startsWith(`$`)) parseCommand(line.slice(2));
+            else {
+                const rSize = line.split(/\s/g)[0];
+                if (rSize !== `dir`) {
+                    const size = parseInt(rSize); 
+                    const dirs = expand(curPath);
+
+                    for (const dir of dirs) tree[dir] += size;
+                }
+            }
+        }
+
+        const ans = Object.values(tree).filter(x => x <= 1e5).reduce((a, b) => a + b);
+
+        console.log(`Result: ${ans}`);
+        fs.writeFileSync(path.resolve(__dirname, `./output.txt`), String(ans));
+    });
+};
+
+void main();
